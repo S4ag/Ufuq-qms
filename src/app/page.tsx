79975934b89dbef5fcd26f11046252f2c87a90be
S4ag <saudaziz43@gmail.com>
2026-07-14 18:43:31 +0000

    fix auth import poaths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@/lib/auth";
+import { auth } from "../lib/auth";
 import { redirect } from "next/navigation";
 export default async function Index() {const session = await auth();if (!session) redirect("/login");if (session.user.role === "ADMIN") redirect("/admin");redirect("/me");
 }
